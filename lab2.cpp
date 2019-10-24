a31e0f73d4bb3f0359b061ed2195a15dd44da5b5
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2019-10-24 20:42:13 +0300

    cmake added

---
+++ lab2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Coordinates() {
+    double firstRadius, secondRadius, firstCoordinateX, secondCoordinateX, firstCoordinateY, secondCoordinateY;
+    cout << "Enter first radius:" << endl;
+    cin >> firstRadius;
+    cout << "Enter second radius:" << endl;
+    cin >> secondRadius;
+
+    cout << "Enter coordinates for first circle:" << endl;
+    cin >> firstCoordinateX >> firstCoordinateY;
+
+    cout << "Enter coordinates for second circle:" << endl;
+    cin >> secondCoordinateX >> secondCoordinateY;
+
+    double newX = (firstCoordinateX * firstRadius + secondCoordinateX * secondRadius) / (firstRadius + secondRadius);
+    double newY = (firstCoordinateY * firstRadius + secondCoordinateY * secondRadius) / (firstRadius + secondRadius);
+    printf("%lf\n, %lf\n", newX, newY);
+}
+
+void Traectory() {
+    int radius, firstCoordinateX, lastCoordinateX, firstCoordinateY, lastCoordinateY, coordinateOfPointX, coordinateOfPointY, CoordinateX, CoordinateY, new_CoordinateY;
+    cout << "Enter  radius:" << endl;
+    cin >> radius;
+    cout << "Enter first coordinates for line:" << endl;
+    cin >> firstCoordinateX >> firstCoordinateY;
+    cout << "Enter last coordinates for line:" << endl;
+    cin >> lastCoordinateX >> lastCoordinateY;
+    cout << "Enter coordinates of circle center :" << endl;
+    cin >> CoordinateX >> CoordinateY;
+    cout << "Enter coordinates of point:" << endl;
+    cin >> coordinateOfPointX >> coordinateOfPointY;
+    int length = ((CoordinateX * CoordinateX) - (coordinateOfPointX * coordinateOfPointX)) +
+                 ((CoordinateY * CoordinateY) - (coordinateOfPointY * coordinateOfPointY));
+    int n = 1 / 2;
+    for (int x = CoordinateX; x < lastCoordinateX; x += 2) {
+        n += n;
+        new_CoordinateY = length * n;
+        printf("%d\n", new_CoordinateY);
+    }
+}
+
+int main() {
+    Coordinates();
+    Traectory();
+    return 0;
+}
